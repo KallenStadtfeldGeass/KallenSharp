0b80cff3eaa288491cd133b4ffcbb8a2e34e305b
Geass <kallenstadtfeldgeass@gmail.com>
2016-09-02 06:59:56 -0700

    h

--- [Project]Geass/[Project]Geass/Functions/Prediction.cs
+++ [Project]Geass/[Project]Geass/Functions/Prediction.cs
@@ -7,12 +7,14 @@ namespace _Project_Geass.Functions
 {
     internal class Prediction
     {
-        private static readonly HitChance[] HitChanceValues = (HitChance[])Enum.GetValues(typeof(HitChance));
-
         public static string[] GetHitChanceNames()
         {
             var names = Enum.GetNames(typeof(HitChance));
-            return names.Where((t, i) => HitChanceValues[i] >= HitChance.Low && HitChanceValues[i] <= HitChance.Immobile).Reverse().ToArray();
+            return new[]
+            {
+                names[(int) HitChance.VeryHigh], names[(int) HitChance.High], names[(int) HitChance.Medium],names[(int) HitChance.Low],
+                names[(int) HitChance.Immobile], names[(int) HitChance.Dashing]
+            };
         }
 
         public static HitChance GetHitChance(string value) => (HitChance)Enum.Parse(typeof(HitChance), value);
