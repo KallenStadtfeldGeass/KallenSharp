e78a5a4ca91efdc7c37f81ae6b23c1054cb20b05
Geass <kallenstadtfeldgeass@gmail.com>
2016-09-02 06:49:43 -0700

    ddd

--- [Project]Geass/[Project]Geass/Functions/Prediction.cs
+++ [Project]Geass/[Project]Geass/Functions/Prediction.cs
@@ -1,13 +1,18 @@
 ﻿using LeagueSharp.Common;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _Project_Geass.Functions
 {
     internal class Prediction
     {
+        private static readonly HitChance[] HitChanceValues = (HitChance[])Enum.GetValues(typeof(HitChance));
+
         public static string[] GetHitChanceNames()
         {
-            return Enum.GetNames(typeof(HitChance));
+            var names = Enum.GetNames(typeof(HitChance));
+            return names.Where((t, i) => HitChanceValues[i] >= HitChance.Low && HitChanceValues[i] <= HitChance.Immobile).Reverse().ToArray();
         }
 
         public static HitChance GetHitChance(string value) => (HitChance)Enum.Parse(typeof(HitChance), value);
